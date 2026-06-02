29c8a41c380dfdeb7f333df789a4f3d0b9f1d227
Trần Mạnh Cường <cuongtm1@newwave.vn>
2026-06-02 16:53:33 +0700

    feat: refactor code structure for improved readability and maintainability

---
+++ src/pages/components/ContactSection/index.tsx
@@ -0,0 +1,19 @@
+import youtubeImg from "../../../assets/images/contact/icons/YoutubeIcon.webp";
+import emailImg from "../../../assets/images/contact/icons/EmailIcon.webp";
+import linkinImg from "../../../assets/images/contact/icons/LinkinIcon.webp";
+import phoneImg from "../../../assets/images/contact/icons/PhoneIcon.webp";
+import whatsappImg from "../../../assets/images/contact/icons/WhatsappIcon.webp";
+import zaloImg from "../../../assets/images/contact/icons/ZaloIcon.webp";
+
+export const ContactSection = () => {
+  return (
+    <>
+      <img src={youtubeImg} />
+      <img src={emailImg} />
+      <img src={linkinImg} />
+      <img src={phoneImg} />
+      <img src={whatsappImg} />
+      <img src={zaloImg} />
+    </>
+  );
+};
