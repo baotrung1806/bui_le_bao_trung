97b6d19145f2724930082f2adb50f1aaf7a2dd85
Trần Mạnh Cường <cuongtm1@newwave.vn>
2026-06-04 11:40:21 +0700

    feat: create JobSection and ProjectSection

--- src/pages/components/JobSection/index.tsx
+++ src/pages/components/JobSection/index.tsx
@@ -1,32 +1,12 @@
-import { ButtonCustom } from '@/components/ButtonCustom';
 import { useTranslation } from '@/utils';
 
-export const MySelfSection = () => {
-  const t = useTranslation('main.myself');
+const JobSection = () => {
+  const t = useTranslation('main.job');
 
   return (
-    <>
-      <div>
-        <form>
-          <p>{t('fullname')}</p>
-          <p>{t('nickname')}</p>
-          <p>{t('address')}</p>
-          <p>{t('education.first.label')}</p>
-          <span>{t('education.first.subLabel')}</span>
-          <p>{t('education.second.label')}</p>
-          <span>{t('education.second.subLabel')}</span>
-          <p>{t('education.third.label')}</p>
-          <span>{t('education.third.subLabel')}</span>
-        </form>
-      </div>
-      <div>
-        <p>{t('headline')}</p>
-        <p>{t('fullname')}</p>
-        <p>{t('title')}</p>
-        <span>{t('subTitle')}</span>
-        <ButtonCustom className="bg-[#E9D4B9] text-[#3C2F2F]" value={t('downloadCV')} />
-        <ButtonCustom className="bg-[#E9D4B9] text-[#3C2F2F]" value={t('contact')} />
-      </div>
-    </>
+    <div className="flex flex-col inset-0 h-screen">
+    </div>
   );
 };
+
+export default JobSection;
---
+++ src/pages/components/ProjectSection/index.tsx
@@ -0,0 +1,12 @@
+import { useTranslation } from '@/utils';
+
+const ProjectSection = () => {
+  const t = useTranslation('main.project');
+
+  return (
+    <div className="flex flex-col inset-0 h-screen">
+    </div>
+  );
+};
+
+export default ProjectSection;
