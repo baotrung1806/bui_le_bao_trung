03f02c7236ced5a175180741b277ab11402a7ffc
Trần Mạnh Cường <cuongtm1@newwave.vn>
2026-06-05 11:07:25 +0700

    fix: hide some line code

--- src/pages/components/ProjectSection/index.tsx
+++ src/pages/components/ProjectSection/index.tsx
@@ -1,7 +1,7 @@
-import { useTranslation } from '@/utils';
+// import { useTranslation } from '@/utils';
 
 const ProjectSection = () => {
-  const t = useTranslation('main.project');
+  // const t = useTranslation('main.project');
 
   return (
     <div className="flex flex-col inset-0 h-screen">
